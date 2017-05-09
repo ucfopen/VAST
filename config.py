8e2ed97ba818065b1b2f80bc3f608bc68e06e040
John Raible <john.raible@ucf.edu>
2017-05-09 12:30:44 -0400

    Upload new file

---
+++ config.py
@@ -0,0 +1,11 @@
+#Enter API key from canvas
+api_key = ""
+#Change to your institutional address
+api_url = ""
+#add api key from youtube
+youtube_key = ''
+google_url = 'https://www.googleapis.com/youtube/v3/captions'
+google_video = 'https://www.googleapis.com/youtube/v3/videos'
+#add vimeo api key
+vimeo_api_key = ''
+youtube_pattern = r'(?:https?:\/\/)?(?:[0-9A-Z-]+\.)?(?:youtube|youtu|youtube-nocookie)\.(?:com|be)\/(?:watch\?v=|watch\?.+&v=|embed\/|v\/|.+\?v=)?([^&=\n%\?]{11})'
