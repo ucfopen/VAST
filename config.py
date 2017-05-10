87e80cb4e837622013e86cab02c5c1aa4ea88b1f
John Raible <john.raible@ucf.edu>
2017-05-10 10:32:04 -0400

    Added Course URL and YouTube Pattern

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-#Enter API key from canvas
 api_key = ""
-#Change to your institutional address
-api_url = ""
-#add api key from youtube
+api_url = "https://webcourses.ucf.edu/api/v1/"
 youtube_key = ''
 google_url = 'https://www.googleapis.com/youtube/v3/captions'
 google_video = 'https://www.googleapis.com/youtube/v3/videos'
-#add vimeo api key
 vimeo_api_key = ''
 youtube_pattern = r'(?:https?:\/\/)?(?:[0-9A-Z-]+\.)?(?:youtube|youtu|youtube-nocookie)\.(?:com|be)\/(?:watch\?v=|watch\?.+&v=|embed\/|v\/|.+\?v=)?([^&=\n%\?]{11})'
+courses_url = 'https://webcourses.ucf.edu/courses'
